18274e229928ff2637316dcb4f6ec753632f4bec
Jason Yates <yatesj@casinorama.com>
2014-08-07 16:17:10 -0400

    Added missing Deps

--- src/leiningen/new/clj_angjs/project.clj
+++ src/leiningen/new/clj_angjs/project.clj
@@ -1,5 +1,5 @@
 (defproject {{ns-name}} "0.1.0"
-  :description "{{ns-name}}"
+  :description "Admin for Vault"
   :url "http://gitlab/{{ns-name}}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,5 +7,10 @@
                  [ring-mock "0.1.5"]
                  [ring-middleware-format "0.3.2"]
                  [com.duelinmarkers/ring-request-logging "0.2.0"]
-                 [compojure "1.1.8"]]
+                 [compojure "1.1.8"]
+                 [log4j/log4j "1.2.17"]
+                 [org.slf4j/slf4j-log4j12 "1.7.7"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [expectations "2.0.9"]
+                 [ring "1.3.0"]]
   :plugins [[lein-autoexpect "1.0"]])
